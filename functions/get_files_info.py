1ebe7e0d083f25e9df50a4fc00cd262c1d0fadcf
AkuPython <12842971+AkuPython@users.noreply.github.com>
2025-06-15 00:18:07 +0930

    CH2 complete - add read file

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,5 @@
 import os
+from config import MAX_CHARS
 
 def get_files_info(working_directory, directory=None):
     if not isinstance(directory, str) or directory == None:
@@ -30,9 +31,13 @@ def get_file_content(working_directory, file_path):
         file_content_string = os.path.abspath(os.path.join(working_directory, file_path))
     if not root in file_content_string:
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-    if not os.path.isfile(file_content_string):
-        return f'Error: File not found or is not a regular file: "{file_path}"'
-    MAX_CHARS = 10000
-    with open(file_path, "r") as f:
-        file_content_string = f.read(MAX_CHARS)
-    return file_content_string
+    try:
+        if not os.path.isfile(file_content_string):
+            return f'Error: File not found or is not a regular file: "{file_content_string}"'
+        with open(file_content_string, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+        if len(file_content_string) == MAX_CHARS:
+            file_content_string = file_content_string[:MAX_CHARS] + f'[...File "{file_path}" truncated at 10000 characters]'
+        return file_content_string
+    except Exception as e:
+        return f'Error: File not found or is not a regular file: "{file_path}", {e}'
---
+++ get_file_content.py
@@ -0,0 +1,19 @@
+import os
+from config import MAX_CHARS
+
+def get_file_content(working_directory, file_path):
+    root = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(root):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(abs_file_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    try:
+        with open(abs_file_path, "r") as f:
+            content = f.read(MAX_CHARS + 1)
+        if len(content) > MAX_CHARS:
+            content = content[:MAX_CHARS] + f'[...File "{file_path}" truncated at 10000 characters]'
+        return content
+    except Exception as e:
+        return f'Error: Issue reading file: "{file_path}", {e}'
+
