76be41af82dd11a1eac70671bb6d5d20c7174aa3
AkuPython <12842971+AkuPython@users.noreply.github.com>
2025-06-14 23:53:43 +0930

    working on CH2

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -23,3 +23,16 @@ def get_files_info(working_directory, directory=None):
     except Exception as e:
         return f"Error listing files: {e}"
 
+def get_file_content(working_directory, file_path):
+    root = os.path.abspath(working_directory)
+    file_content_string = root
+    if file_path:
+        file_content_string = os.path.abspath(os.path.join(working_directory, file_path))
+    if not root in file_content_string:
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(file_content_string):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    MAX_CHARS = 10000
+    with open(file_path, "r") as f:
+        file_content_string = f.read(MAX_CHARS)
+    return file_content_string
--- tests.py
+++ tests.py
@@ -1,8 +1,11 @@
-from functions.get_files_info import get_files_info
+from functions.get_files_info import get_file_content, get_files_info
 
 
 if __name__ == "__main__":
-    print(get_files_info("calculator", "."))
-    print(get_files_info("calculator", "pkg"))
-    print(get_files_info("calculator", "/bin"))
-    print(get_files_info("calculator", "../"))
+    # print(get_files_info("calculator", "."))
+    # print(get_files_info("calculator", "pkg"))
+    # print(get_files_info("calculator", "/bin"))
+    # print(get_files_info("calculator", "../"))
+    print(get_file_content("calculator", "main.py"))
+    print(get_file_content("calculator", "pkg/calculator.py"))
+    print(get_file_content("calculator", "/bin/cat"))
